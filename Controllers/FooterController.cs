61fe6dd1a8ea8a6e28df4454f2d0dd59c74123a7
Nguyen Long <longnguyenhoang.ict@gmail.com>
2023-10-03 07:55:28 +0700

    update footer

--- Controllers/FooterController.cs
+++ Controllers/FooterController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HDU_Website.Models;
 
 namespace HDU_Website.Controllers
 {
     public class FooterController : Controller
     {
+        DBConnection dbConnection = new DBConnection();
         // GET: Footer
         public ActionResult Index()
         {
-            return PartialView();
+            var rightNews = dbConnection.SYS_CaiDatCauHinh.Where(n => n.KeyCauHinh.Equals("TH1065DF_Footer_Info") && n.ForWeb == 1).FirstOrDefault();
+            return PartialView(rightNews);
         }
     }
 }
